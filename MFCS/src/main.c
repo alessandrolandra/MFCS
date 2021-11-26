6549741804658ee3f8d273f3a13fc3ca95519481
Alessandro Landra <landra.alessandro@gmail.com>
2021-11-26 20:05:05 +0100

    platformio project added

---
+++ MFCS/src/main.c
@@ -0,0 +1,21 @@
+#include <stm32f411xe.h>
+
+void busy_delay(int ms);
+
+int main(){
+  RCC->AHB1ENR |= 1;
+  GPIOA->MODER &= ~0x00000C00;
+  GPIOA->MODER |=  0x00000400;
+  while(1){
+    GPIOA->ODR |= 0x00000020;
+    busy_delay(250);
+    GPIOA->ODR &= ~0x00000020;
+    busy_delay(250);
+  }
+}
+
+void busy_delay(int ms){
+  int i;
+  for (; ms >0 ; ms--)
+    for(i=0;i<3195;i++);
+}
